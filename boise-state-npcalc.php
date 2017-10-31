2f50dc2daf4e65d79811c0d46a546a47389d929d
davidmlentz <davidmlentz@gmail.com>
2017-10-31 16:03:23 -0600

    Reduce width of the container

--- boise-state-npcalc.php
+++ boise-state-npcalc.php
@@ -3,7 +3,7 @@
 Plugin Name: Boise State Net Price Calculator
 Plugin URI: https://github.com/OITWPsupport
 Description: Net Price Calculator code, based on https://nces.ed.gov/ipeds/netpricecalculator/ and customized for Boise State University and to correct a11y errors.
-Version: 0.0.4
+Version: 0.0.5
 Author URI: https://webguide.boisestate.edu/
 */
 
